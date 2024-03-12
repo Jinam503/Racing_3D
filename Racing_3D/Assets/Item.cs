b96d89081afae81e24e9cdac973d59dceb28d71a
백진암 <jinam1234567@gmail.com>
2024-03-12 20:39:04 +0900

    ADD: RandomBox

---
+++ Racing_3D/Assets/Item.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag("Car"))
+            return;
+        float randomValue = Random.value * 100f; // 0에서 100 사이의 랜덤 값 생성
+
+        switch (randomValue)
+        {
+            case < 5f:// 코인 1000
+                Debug.Log("10% 확률로 동작 1");
+                break;
+            case < 15f:// 코인 500
+                Debug.Log("10% 확률로 동작 2");
+                break;
+            case < 60f: // 랜덤 사용 아이템 - 미사일, 범퍼- 부딮히면 상대차 뒤집힙.
+                Debug.Log("20% 확률로 동작 3");
+                break;
+            default: //코인 100
+                Debug.Log("30% 확률로 동작 5");
+                break;
+        }
+    }
+}
+
